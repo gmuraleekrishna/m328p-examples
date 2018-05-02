f4c80c025dca6f733f1a9bcae5687916565f773a
Muraleekrishna Gopinathan <muraleekrishna@protonmail.ch>
2018-05-02 13:52:06 +1000

    Update 3_uart_tx_str.c

--- 3_uart_tx_str.c
+++ 3_uart_tx_str.c
@@ -1,13 +1,3 @@
-/*
- * 3. uart_tx_str.c
- *
- *	Objective:  Understand the UART (universal asynchronous receiver and transmitter)
- *				We will use the UART to transmit a string data 
- *
- * Created: 2/9/2018 9:32:26 PM
- * Author : Jon Kim
- */ 
-
 #define F_CPU	20000000
 
 #define BAUD_RATE	9600
